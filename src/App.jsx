dd35a150890c6feceafcb420d5620f330bc24fe6
Santiago Mariani <santi@gospartanburg.com>
2024-07-30 18:56:58 -0400

    added personal content

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import santiImage from './assets/santi.jpg';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>SANTIVILLE</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <img src={santiImage} className="logo" alt="React logo" />
+      <h1>Hello, World!</h1>
+      <h2>It's Santi :)</h2>
+      <p></p>
+      <h2>Hire me or I'll starve :(</h2>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        santi@gospartanburg.com • 704-777-1494
       </p>
     </>
   )
