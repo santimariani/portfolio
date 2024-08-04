de77f0e424c4ae5abf19e4d809c579200e37a42c
Santiago Mariani <santi@gospartanburg.com>
2024-08-04 16:40:51 -0400

    added images

--- src/Routes/Projects.jsx
+++ src/Routes/Projects.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
+import "../CSS/Projects.css";
 
 const Projects = () => {
   return (
-    <div>
-      <p>Welcome to the About page! Here we share information about our team and mission.</p>
+    <div id="projectsGrid">
+      <section id="carouselGrid">
+        <div className="square">
+          <img src="/100Days.png" alt="Python RPG Project" />
+        </div>
+        <div className="square">
+          <img src="/magic8.png" alt="Magic 8-Ball Project" />
+        </div>
+        <div className="square">
+          <img src="/theater.png" alt="Theater API Project" />
+        </div>
+        <div className="square">
+          <img src="/robinsonCrusoe.png" alt="Robinson Crusoe Project" />
+        </div>
+        <div className="square">
+          <img src="/ultimateFighters2.png" alt="JS Video Game Project" />
+        </div>
 
+      </section>
+      <section id="projectCanvas">
+        <div id="projectDisplay"></div>
+      </section>
+      <section id="projectDescription">
+        <p>TECHNOLOGIES USED: THIS • THAT • THIS & THAT • THAT & THIS</p>
+      </section>
     </div>
   );
 };
