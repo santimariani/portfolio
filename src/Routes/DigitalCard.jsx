157d9c4afe348f36ddc32ca85164c7f82d3a6eaf
Santiago Mariani <santi@gospartanburg.com>
2024-08-08 14:02:46 -0400

    contact me fixed

--- src/Routes/DigitalCard.jsx
+++ src/Routes/DigitalCard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import MarianiDigitalCard from "/MarianiDigitalCard.png";
 
-import "../CSS/DigitalCard.css"
+import "../CSS/DigitalCard.css";
 
 const DigitalCard = () => {
   return (
     <div className="center">
       <p id="digitalCardTitle">SCAN FOR CONTACT</p>
       <img src={MarianiDigitalCard} id="digitalCard"></img>
-      <a href="https://santimariani.com/contact-me" className="contactButton">
+      <a
+        href="https://www.santimariani.com/contact-me"
+        className="contactButton"
+      >
         OR CONTACT ME HERE
       </a>
     </div>
