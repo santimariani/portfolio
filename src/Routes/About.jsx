9cd598d951bfa22e4e943050509911ae3596153c
Santiago Mariani <santi@gospartanburg.com>
2024-08-18 23:58:56 -0400

    updated bio:

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -5,11 +5,12 @@ const About = () => {
   return (
     <div>
       <p className="left" style={{ padding: "3vh 4vh" }}>
-        <span className="bold">HELLO, THERE!</span>
+        <span className="bold">WHY, HELLO THERE! :)</span>
         <br></br>
         <br></br>
-        My name is Santi, a full-stack developer with a love for both beauty and
-        logic and an appetite for continual learning and growth.
+        My name is Santi, a full-stack developer with a deep love for beauty and
+        logic, a curious eye for innovation and problem-solving, and an
+        insatiable appetite for continual learning and growth!
         <br></br>
         <br></br>
         An alumnus of the <span className="italics">Carolina Code School</span>,
@@ -19,50 +20,211 @@ const About = () => {
         <br></br>
         <br></br>
         For the front-end, I was equipped with JavaScript, React, HTML, and CSS.
-        For the back-end, Python, FastAPI, PostgreSQL, and Supabase became my friends. On
-        the horizon, Next.js, TypeScript, and Tailwind beckon me onward. Not a
-        dull moment is expected at any turn!
+        For the back-end, Python, FastAPI, PostgreSQL, and Supabase became my
+        tools-to-go. On the horizon, Next.js, TypeScript, and Tailwind beckon me
+        onward. Not a single dull moment is expected at -any- turn.
         <br></br>
         <br></br>
-        User experience is at the forefront of every project — unique
+        User experience is at the forefront of my every project—unique
         interfaces, an ever-passionate goal! And beyond the visible, I enjoy
-        employing sound logic to create solid apps that go the above-and-beyond.
-        For more on the evolution of my software development journey, see my{" "}
-        <Link to="/projects">Projects</Link> below!
+        employing sound logic and best principles to create solid apps that go
+        above-and-beyond. For more on the evolution of my software development
+        journey, see my <Link to="/projects">Projects</Link> below!
         <br></br>
         <br></br>
+        With the essentials taken care of, feel free now to keep on reading at
+        your own peril, for to do so will mean going knee-deep into the realm of
+        Santi Lore. Think twice before doing it! Yet not thrice, lest drama
+        queens get bored.
         <br></br>
-        <span className="bold">MY JOURNEY:</span>
+        <br></br>
+        <span className="bold">"DON'T CRY FOR ME..."</span>
         <br></br>
         <br></br>
         Originally born in Argentina (
         <span className="italics">hablo español</span>), I moved to the United
-        States in 1996, falling in love with the culture of my new home. I went
-        to Wofford College, majoring{" "}
-        <span className="italics">summa cum laude</span> in Finance, and then
-        devoted the next ten years to non-profit service work, locally and
-        abroad. Grateful for these years of service, I returned to my beloved
-        Spartanburg, to be closer to family, friends, and home.
+        States in 1996 with my parents and one brother, falling in love with my
+        new home. While learning a new language had its immense challenges, it
+        taught me the importance of consistency and rewarded me with the
+        capacity to understand life through two diverse cultural "modes." As an
+        immigrant family, we learned to take nothing for granted — there were no
+        generational hand-me-downs; only what could be earned through sweat,
+        tears of sacrifice, and hard work.
         <br></br>
         <br></br>
-        <br></br>
-        <span className="bold">PROFESSIONAL GROWTH:</span>
-        <br></br>
-        <br></br>
-        Three years ago, I became a realtor and a rapidly growing content
-        creator, learning the ins and outs of impromptu marketing. Thanks to my
-        efforts and local contributions, I thus become a Planning Commissioner
-        for the City of Spartanburg, an Ambassador for OneSpartanburg, Inc.'s
-        "Power Up" entrepreneurship program, and a source and point of contact
-        for the local news.
+        Despite the language barrier, I graduated from Spartanburg High School
+        as a Palmetto Fellow Scholar, being in the top 3% of my class. I took
+        voice lessons and was <span className="italics">painfully</span>{" "}
+        socially awkward. Above all, I made friendships that, since then, have
+        only grown. For I am one fortunate soul.
         <br></br>
         <br></br>
-        Yet what's the point of all work and no fun?! Hence, while juggling all
-        this, I also became a{" "}
-        <span className="italics">Spartanburg Cocktail Masters</span> annual
-        event co-creator, helping to draw in hundreds of people to our beautiful
-        city and raising tens of thousands of dollars for a local cause. Keep
-        your eyes on our{" "}
+        <span className="bold">ENTER—WOFFORD</span>
+        <br></br>
+        <br></br>
+        One of my greatest joys in these early years is having attended Wofford
+        College, majoring <span className="italics">summa cum laude</span> in
+        Finance. There, I became a student leader through the Success Initiative
+        program, where I began to understand the fundamentals of
+        entrepreneurship. In practice, I discovered within the program a need
+        for more efficient communication between the various groups. To solve
+        this unperceived need, I created a tailored online-forum — facilitating
+        real-time conversations and saving the program thousands of dollars.
+        Hence, I earned the{" "}
+        <span className="italics">
+          Dr. James Proctor Award for Innovation, Creativity, and
+          Entrepreneurship
+        </span>
+        ... and a trend for creative problem-solving was born!
+        <br></br>
+        <br></br>
+        (I also continued performing as a singer during this time and was even
+        the protagonist in a children's opera:{" "}
+        <span className="italics">Sid the Serpent Who Wanted to Singp</span>.
+        But as I had to wear a leotard for this one, we do not talk about this,
+        nay, not even in whispers. Who even -is- this Sid fellow of old?!)
+        <br></br>
+        <br></br>
+        Balancing my creative outlets, my financial classes helped me to look at
+        challenges through more of an analytical mindset. Attention to detail
+        became a constant necessity, the application of mathematics a daily
+        must. "How boring," some might say. Yet I loved every moment of it! And
+        so I earned the{" "}
+        <span className="italics">
+          Departmental Award for Academic Achievement, Scholarly Promise, and
+          Intellectual and Moral Integrity
+        </span>
+        . I am thus undeniably and irrevocably a 100% certifiable (and
+        certified!) nerd, geek, and dork.
+        <br></br>
+        <br></br>
+        <span className="bold">TWO ROADS DIVERGE</span>
+        <br></br>
+        <br></br>
+        To conclude my time at Wofford, I was selected to go to the Aspen
+        Institute as an intern — a first for Wofford, "so make us shine like we
+        are among the best!" There, I met world leaders as I did various tasks
+        to support the operations of this globally renowned think-tank. On the
+        one hand, I was amazed by the human capacity for progress. On the other,
+        I noticed how many of the world's wealthiest were still left grappling
+        for a deeper meaning to it all.
+        <br></br>
+        <br></br>
+        "We have everything yet are still unhappy," it was mentioned at a
+        private group gathering. "For this problem and answer is yet to be
+        found."
+        <br></br>
+        <br></br>
+        Until then, I had been aspiring to be 'financially free.' This was the
+        path to which I had given my all! It was why I had majored in finance.
+        It was why I did my every project. It was why I had accepted this
+        internship when even asked at all...
+        <br></br>
+        <br></br>
+        Yet after hearing that admission, I was forever a changed man, having
+        been spared decades of mindless wandering. My heart thus embarked on a
+        different adventure. A new path became trod. 
+        <br></br>
+        <br></br>
+        <span className="bold">IN PURSUIT OF A GREATER LOVE</span>
+        <br></br>
+        <br></br>
+        Much could be written about the next ten years of my life. Yet as surely
+        only my mother is reading at this point, I will only say how blessed I
+        am to have been gifted this decade of service, "non-profit" work.
+        <br></br>
+        <br></br>
+        For ten years I got to travel the world, going from Pennsylvania to
+        Italy then to Africa and finally to Carolina of North. I tried hot wine
+        in a wintry Poland. I touched Michelangelo's{" "}
+        <span className="italics">Pieta</span> supposedly protected behind thick
+        glass walls.
+        <br></br>
+        <br></br>
+        For ten years I got to tend to the actively dying, rejoice at the birth
+        of newborns, give a clumsy word or two to those plunged in sorrow.
+        <br></br>
+        <br></br>For ten years I saw people come alive after years of internal
+        drudgery; eyes light up after decades of glimpsing in strained darkness;
+        tears roll down cheeks once covered in spiritual soot.
+        <br></br>
+        <br></br>
+        Time and time again, others showed me that there is goodness to be
+        found, even in the most difficult of circumstances. Those of different
+        cultures showed me how we all have one same intrinsic longing... a
+        common and equal dignity... a single human fabric from which we are
+        weaved and made to grow.
+        <br></br>
+        <br></br>
+        And thus I returned to my beloved Spartanburg after those ten years,
+        thankful for the graces received, thankful to once again be among family
+        and friends... thankful to once again be among familiar roots from which
+        sprung "home."
+        <br></br>
+        <br></br>
+        <span className="bold">TWO ROADS CONVERGE</span>
+        <br></br>
+        <br></br>
+        The year was 2020, and the world, for a while, turned off. I played a
+        lot of Stardew Valley and afterwards pivoted towards a new career. I
+        became a realtor, to wed the previous path of finance with the more
+        current one of service work.
+        <br></br>
+        <br></br>
+        As a realtor, I had to learn to understand my clients' true needs, going
+        from the surface "what's" that were presented to the deeper "why's" from
+        which satisfaction is born. Doing this full-time meant learning to
+        become adept at handling multiple "projects" simultaneously,
+        orchestrating deals and practicalities to help them reach that end-dream
+        goal. With each success, I sought to then do it better. With each
+        dropped deal, I asked myself where I could have done better, or more.
+        <br></br>
+        <br></br>
+        Having returned to social media as a professional necessity, I rapidly
+        grew as a content creator. I sought to only provide what was of value as
+        I learned the ins and outs of impromptu marketing. I became a local
+        voice. And hence, thanks to my efforts and local contributions, I became
+        a Planning Commissioner for the City of Spartanburg, one of
+        OneSpartanburg, Inc.'s (the 2021 US Chamber of Commerce) first
+        Ambassadors for their "Power Up" entrepreneurship program, and a source
+        and point of contact for the local news.
+        <br></br>
+        <br></br>
+        Toes were stepped on, lessons learned, and new leadership skills
+        acquired as I considered and weighed in the needs of individuals in
+        contrast to the needs of the whole.
+        <br></br>
+        <br></br>
+        So many unique opportunities. I am thankful for it all!
+        <br></br>
+        <br></br>
+        <span className="bold">AND NOW... A HAPPY TOAST</span>
+        <br></br>
+        <br></br>
+        Yet what would be the point of all work and no fun?! Hence, while
+        juggling all this, I also became a co-creator for the{" "}
+        <span className="italics">Spartanburg Cocktail Masters</span>... and at
+        the prompting, no less, of a cop! And so, hundreds of people have been
+        drawn in these last two years to our beautiful Spartanburg to taste the
+        samples of 15 local competitors — who in turn battle it out to be the
+        next Spartanburg Cocktail Master, all to support a local cause. For
+        Spartanburg, if you haven't noticed, downright rocks.
+        <br></br>
+        <br></br>
+        The creation of this annual event helped me learn so many lessons—both
+        as a leader and simultaneously a member of the group—especially as it
+        pertained to managing priorities, expectations, and deadlines. Sponsors,
+        vendors, competitors, venues, judges, licenses, insurance, laws, and so
+        much more had to be juggled to bring this event to life! And so I
+        learned to delegate what I could not do to those who could, and what I{" "}
+        <span className="italics">could</span> do to those who could do it{" "}
+        <span className="italics">better</span>. Due to its success, the event
+        has been adopted by a state-wide organization, which -itself- was a new
+        challenge, yet a most worthwhile one, that the event may grow for years
+        to come.
+        <br></br>
+        <br></br>
+        So keep your eyes on our{" "}
         <a
           href="https://burgcocktailmasters.com/"
           target="_blank"
@@ -73,8 +235,7 @@ const About = () => {
         every August to join in on the fun!
         <br></br>
         <br></br>
-        <br></br>
-        <span className="bold">A NEW ADVENTURE BEGINS:</span>
+        <span className="bold">TO NEW ADVENTURES!</span>
         <br></br>
         <br></br>
         In the midst of such intense activity, I ended up discovering a true
@@ -82,13 +243,13 @@ const About = () => {
         life. My observant brother (a classy geek and bona fide dork) encouraged
         me to take a closer look at coding, knowing it would wash with so many
         fun chemicals my ADHD brain, oh-so-OCD prone. And so I became — at long
-        happy last — a software developer, ready to bring greater beauty and order
-        to the world through code.
+        happy last — a software developer, ready to bring greater beauty and
+        order to the world through code.
         <br></br>
         <br></br>
         In my spare time, I enjoy hiking with friends, watching others play
-        Minecraft, and -not- talking long walks on the beach, lest the sand get
-        in in-every-which-spot.
+        Minecraft, and <span className="italics">not</span> taking long walks on
+        the beach, lest the sand get in in-every-which-spot.
         <br></br>
         <br></br>
         Thank you for taking the time to get to know me! Now join me on my
@@ -97,7 +258,7 @@ const About = () => {
         to come.
         <br></br>
         <br></br>
-        <span className="bold">Enjoy your stay!</span> :)
+        <span className="bold">ENJOY YOUR STAY :D</span>
       </p>
     </div>
   );
